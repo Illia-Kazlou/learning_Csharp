29045b2662d860cea42769efd8f4d2fd33e4df37
Hamidalion <fearofwar@mail.ru>
2020-12-09 21:52:36 +0300

    feat: lern prop

---
+++ src/9_Class-Constr-Prop/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+namespace _9_Class_Constr_Prop
+{
+    public class Person
+    {
+        private string _name;
+
+        public string SecondName { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Имя не может быть пустым");
+                }
+                _name = value + "+Maick";
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return SecondName + " " + Name;
+            }
+        }
+
+        public string ShortName
+        {
+            get
+            {
+                return $"{SecondName} {Name.Substring(0, 1)}.";
+            }
+        }
+
+        ///тоже что и вверху
+
+        //public string GetName()
+        //{
+        //    return _name;
+        //}
+
+        //public void SetName(string name)
+        //{
+        //    if (string.IsNullOrWhiteSpace(name))
+        //    {
+        //        throw new ArgumentNullException("Имя не может быть пустым");
+        //    }
+        //    _name = name;
+        //}
+
+
+    }
+}
--- src/9_Class-Constr-Prop/Program.cs
+++ src/9_Class-Constr-Prop/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
+
 namespace _9_Class_Constr_Prop
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p = new Person();
+
+            p.Name = "Nick";
+            p.SecondName = " Bond";
+
+            Console.WriteLine(p.Name+p.SecondName);
+
+            Console.WriteLine(p.FullName);
+
+            Console.WriteLine(p.ShortName);
+
+            //p.SetName("Micke");
+            //Console.WriteLine(p.GetName());
+
+            Console.ReadLine();
         }
     }
 }
