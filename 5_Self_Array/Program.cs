7805a284ff3000462222516ed30d47340a905950
Hamidalion <fearofwar@mail.ru>
2020-12-16 21:29:29 +0300

    feat: add 5 lesson learning of array

---
+++ 5_Self_Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _5_Self_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayStart = new int[] { 1, 2, 3, 4, 5 };
+            int n = arrayStart.Length;
+
+            int[] arrayFinish = new int[n];
+
+            int summ = 0;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                summ += arrayStart[i];
+                arrayFinish[i] = summ;
+            }
+            Console.WriteLine(string.Join(" ", arrayFinish));  
+
+            Console.ReadLine();
+        }
+    }   
+}
