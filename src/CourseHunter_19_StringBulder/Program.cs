74993ea67fe0224379e136c2252a77f0ac828f0c
Illia Kozlov <fearofwar@mail.ru>
2021-02-03 17:04:33 +0300

    add StringBuilder.

---
+++ src/CourseHunter_19_StringBulder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace CourseHunter_19_StringBulder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            //метод дабавления в стинг билдер новой строки.
+            sb.Append("My ");
+            sb.Append("name ");
+            sb.Append("is ");
+            sb.Append("John ");
+            //Метод добавление с новой строчик
+            sb.AppendLine("!");
+
+            Console.WriteLine(sb);
+
+            //Метод загона все в строкую 
+            string str = sb.ToString();
+            Console.WriteLine(str);
+        }
+    }
+}
