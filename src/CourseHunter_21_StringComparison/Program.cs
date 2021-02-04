9f907b0e3d3355d854ad10f855b2dcb367985a90
Illia Kozlov <fearofwar@mail.ru>
2021-02-04 14:15:45 +0300

    rename solution & mkdir & add String Comparison

---
+++ src/CourseHunter_21_StringComparison/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CourseHunter_21_StringComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "abcde";
+            string str2 = "abcde";
+
+            bool areEqual = str1 == str2;
+            Console.WriteLine(areEqual);
+        }
+    }
+}
