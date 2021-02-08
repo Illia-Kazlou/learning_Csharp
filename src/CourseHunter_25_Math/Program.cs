c1c4939ff236e2221253396f61625e8c555835d8
Illia Kozlov <fearofwar@mail.ru>
2021-02-08 10:47:56 +0300

    add CourseHunter_25_Math

---
+++ src/CourseHunter_25_Math/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CourseHunter_25_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(2, 3)); // число в степень
+            Console.WriteLine(Math.Sqrt(8)); // корень числа
+            Console.WriteLine(Math.Sqrt(9));
+
+            Console.WriteLine(Math.Round(1.4)); // округление
+            Console.WriteLine(Math.Round(1.5));
+            Console.WriteLine(Math.Round(1.7));
+            Console.WriteLine();
+
+            Console.WriteLine(Math.Round(1.5, MidpointRounding.AwayFromZero));  //округление
+            Console.WriteLine(Math.Round(1.5, MidpointRounding.ToEven));
+            Console.WriteLine(Math.Round(1.5, MidpointRounding.ToNegativeInfinity));
+            Console.WriteLine(Math.Round(1.5, MidpointRounding.ToPositiveInfinity));
+        }
+    }
+}
