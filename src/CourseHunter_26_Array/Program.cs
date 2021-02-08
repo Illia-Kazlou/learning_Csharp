ea8f95943a61f91b5a7658c9c7537bedf759ad12
Illia Kozlov <fearofwar@mail.ru>
2021-02-08 17:17:25 +0300

    add CourseHunter_27_DataTime

---
+++ src/CourseHunter_26_Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CourseHunter_26_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1; //задекларировали переменную
+            array1 = new int [10]; //инициализировали массив
+
+            int[] array2 = new int[5];
+
+            int[] array3 = new int[5] { 2, 4, 6, 8, 10 };
+
+            int[] array4 = { 3, -10, 20, 12, 14 };
+
+            Console.WriteLine(array4[1]);
+
+            int number = array4[3];
+            Console.WriteLine(number);
+
+            Console.WriteLine(array4.Length); //длинна масива 
+            Console.WriteLine(array4[array4.Length-1]); //обращение к крайнему индексу массива
+
+            string str1 = new string("qwerty"); //строку можно представитиь как массив char'ов
+            char first = str1[0]; // обращение к первому символу в строке
+            char last = str1[str1.Length - 1]; // обращение к последнему символу в строке
+
+            Console.WriteLine($"first element is {first}. The last element id {last}");
+
+            //impossible
+            //str1[str1.Length - 1] = 'r';
+        }
+    }
+}
---
+++ src/CourseHunter_27_DataTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CourseHunter_27_DataTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now; // свойство показывающее сегоднящнюю дату и время
+
+
+            Console.WriteLine("Now is {0}", now);
+            Console.WriteLine($"Now is {now.Hour}:{now.Minute} time, {now.Day}.{now.Month}.{now.Year}");
+            Console.WriteLine(new string('-', 30));
+
+            DateTime dt = new DateTime(2016, 2, 28);
+            DateTime newDt = dt.AddYears(4);  // добавление даты 
+            Console.WriteLine(newDt);
+            Console.WriteLine(new string('-', 30));
+
+            TimeSpan ts = now - dt;
+            Console.WriteLine(ts);  // as like ts=now.Subtract(dt)
+            Console.WriteLine(new string('-', 30));
+
+        }
+    }
+}
