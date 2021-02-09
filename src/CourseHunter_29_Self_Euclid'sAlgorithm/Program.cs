c54f5b521770fbd752f71d5ea20dc80b84ac6db8
Illia Kozlov <fearofwar@mail.ru>
2021-02-09 13:38:57 +0300

    add CourseHunter_29_Self_Euclid_sAlgorithm whith&whithout recursion

---
+++ src/CourseHunter_29_Self_Euclid'sAlgorithm/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CourseHunter_29_Self_Euclid_sAlgorithm
+{
+    class Program
+    {
+        /// <summary>
+        /// Algoritm help to find the max common divider (делитель).
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('-', 30));
+
+            //while ((firstNumber != 0) && (secondNumber != 0))
+            //{
+            //    if (firstNumber > secondNumber)
+            //    {
+            //        firstNumber %= secondNumber;
+            //    }
+            //    else
+            //    {
+            //        secondNumber %= firstNumber;
+            //    }
+            //}
+
+            Console.WriteLine("NOD is {0}", GetNODRecurs(secondNumber, firstNumber % secondNumber));
+
+            static int GetNODRecurs(int firstNumber, int secondNumber)
+            {
+                if (secondNumber == 0)
+                {
+                    return firstNumber;
+                }
+                else
+                {
+                    return GetNODRecurs(secondNumber, firstNumber % secondNumber);
+                }
+            }
+
+            
+
+            //if (firstNumber == 0)
+            //{
+            //    Console.WriteLine("NOD is {0}", GetNODRecurs(secondNumber));
+            //}
+            //if (secondNumber == 0)
+            //{
+            //    Console.WriteLine($"NOD is {firstNumber}");
+            //}
+        }
+    }
+}
