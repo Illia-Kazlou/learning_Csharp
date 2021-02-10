2c98b72b349a8ca51a58423fccb84c4b37e10c1a
Illia Kozlov <fearofwar@mail.ru>
2021-02-10 11:01:38 +0300

    add CourseHunter_32_Self_MaxValue

---
+++ src/CourseHunter_32_Self_MaxValue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CourseHunter_32_Self_MaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetWindowSize(Console.WindowWidth, 40);
+            Console.SetWindowSize(Console.WindowHeight, 30);
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('_', 30));
+
+            int max = a;
+            if (b > a)
+            {
+                max = b;
+            }
+            Console.WriteLine($"1. Max = {max}");
+            Console.WriteLine(new string('_', 30));
+
+            int mAx;
+            if (a > b)
+            {
+                mAx = a;
+               
+            }
+            else
+            {
+                mAx = b;
+                
+            }
+            Console.WriteLine($"2. Max = {mAx}");
+            Console.WriteLine(new string('_', 30));
+
+            int maX = a > b ? a : b;
+            Console.WriteLine($"3. Max = {maX}");
+        }
+    }
+}
