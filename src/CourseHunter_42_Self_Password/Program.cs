dbce75edac77aa1578755cdd1b0841dfb03b7e30
Illia Kozlov <fearofwar@mail.ru>
2021-02-11 17:07:14 +0300

    add CourseHunter_42_Self_Password

---
+++ src/CourseHunter_42_Self_Password/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CourseHunter_42_Self_Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetWindowSize(Console.WindowWidth, 50);
+            Console.SetWindowSize(Console.WindowHeight, 60);
+
+            string login = "John";
+            string password = "qwetry";
+
+            int countTry = 1;
+
+            while (countTry <= 3)
+            {
+                Console.WriteLine("Enter login");
+                string enterLogin = Console.ReadLine();
+
+                Console.WriteLine("Enter Password");
+                string enterPasword = Console.ReadLine();
+
+                if (login == enterLogin && enterPasword == password)
+                {
+                    Console.WriteLine("Welcom to system");
+                    break;
+                }
+                else
+                {
+                    countTry++;
+                }
+            }
+            if (countTry == 4)
+            {
+                Console.WriteLine("Your exceeded the number of tries");
+            }
+        }
+    }
+}
