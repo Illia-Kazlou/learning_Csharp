498a5c7ee867fa5d58316ae96f0af8c11397b904
Illia Kozlov <fearofwar@mail.ru>
2021-02-11 15:34:22 +0300

    add CourseHunter_38_DebagMettigMethod

---
+++ src/CourseHunter_38_DebagMettigMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CourseHunter_38_DebagMettigMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's try to calculate the area of a triangle on three sides");
+
+            Console.WriteLine("Enter the length of side AB");
+            double sizeSideAB = GetSide();
+
+            Console.WriteLine("Enter the length of side BC");
+            double sizeSideBC = GetSide();
+
+            Console.WriteLine("Enter the length of side CA");
+            double sizeSideCA = GetSide();
+
+            //Semiperimeter
+            double semiperimeter = (sizeSideAB + sizeSideBC + sizeSideCA) / 2;
+
+            //Area formula gerona
+            double s = Math.Sqrt(semiperimeter * (semiperimeter - sizeSideAB) * (semiperimeter - sizeSideBC) * (semiperimeter - sizeSideCA));
+
+            Console.WriteLine($"Area a triangle is {s}");
+
+            Console.ReadLine();
+        }
+
+        static double GetSide()
+        {
+            return double.Parse(Console.ReadLine());
+        }
+
+    }
+}
