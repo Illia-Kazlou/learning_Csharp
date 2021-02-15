1340ed64faeab4bfc38d09e73f06a59b57de195b
Illia Kozlov <fearofwar@mail.ru>
2021-02-15 19:21:49 +0300

    add CourseHunter_51_Classes

---
+++ src/CourseHunter_51_Classes/Character.cs
@@ -0,0 +1,37 @@
+﻿namespace CourseHunter_51_Classes
+{
+    class Character
+    {
+        //public
+        //internal
+        //protected
+
+        private int health = 100;  //полке класса, по сути переменная
+
+        public int Health // свойства
+        {
+            get
+            {
+                return health;
+            }
+            private set
+            {
+                health = value;
+            }
+        }
+
+        // метод созданный вне класса в других языках программирования называется функцией.
+        // в С# только методы.
+        public void Hit(int damage)
+        {
+            if (damage > damage)
+            {
+                damage = health;
+            }
+
+            //health -= damage;
+
+            Health -= damage;
+        }
+    }
+}
---
+++ src/CourseHunter_51_Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CourseHunter_51_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character c = new Character();
+            c.Hit(10);
+
+
+            Console.WriteLine($"Hello World! {c.Health}");
+
+            Console.ReadLine();
+        }
+    }
+}
