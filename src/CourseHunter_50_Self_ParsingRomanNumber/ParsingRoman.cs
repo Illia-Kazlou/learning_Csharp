423e50c8968bc21ea12f2de19f22a4af27fbc802
Illia Kozlov <fearofwar@mail.ru>
2021-02-15 17:16:51 +0300

    add CourseHunter_50_Self_ParsingRomanNumber

---
+++ src/CourseHunter_50_Self_ParsingRomanNumber/ParsingRoman.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace CourseHunter_50_Self_ParsingRomanNumber
+{
+    class ParsingRoman
+    {
+        private static Dictionary<char, int> map = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5},
+            { 'X', 10 },
+            { 'L',50 },
+            { 'C',100 },
+            { 'D',500 },
+            { 'M',1000 },
+        };
+
+        public static int Parsing(string roman)
+        {
+            int result = 0;
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if (i + 1 < roman.Length && IsSubtractive(roman[i], roman[i + 1]))
+                {
+                    char letter = roman[i];
+                    result -= map[letter];
+                }
+                else
+                {
+                    char letter = roman[i];
+                    result += map[letter];
+                }
+            }
+            return result;
+        }
+
+        private static bool IsSubtractive(char v1, char v2)
+        {
+            return map[v1] < map[v2];
+        }
+    }
+}
---
+++ src/CourseHunter_50_Self_ParsingRomanNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CourseHunter_50_Self_ParsingRomanNumber
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ParsingRoman.Parsing("LXIV")); 
+
+            Console.ReadLine();
+        }
+    }
+}
