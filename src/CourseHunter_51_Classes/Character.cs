a995b1fab97940387c1f04cf7430f49f37b26ccc
Illia Kozlov <fearofwar@mail.ru>
2021-02-18 16:13:55 +0300

    add Methods and method overload

--- src/CourseHunter_51_Classes/Character.cs
+++ src/CourseHunter_51_Classes/Character.cs
@@ -2,13 +2,17 @@
 {
     class Character
     {
+        // Access modifier:
+
+        // private
         //public
-        //internal
+        //internal используется только внутри той сборки в которой он объявлен.
         //protected
 
-        private int health = 100;  //полке класса, по сути переменная
+        //полке класса, по сути переменная
+        public int health = 100;  
 
-        public int Health // свойства
+        public int Health
         {
             get
             {
@@ -20,18 +24,30 @@
             }
         }
 
+        // При компиляции свойсва преобразуются в методы GET & SET
+        //public int GetHealth()
+        //{
+        //    return health;
+        //}
+
+        //public void SetHealth(int value)
+        //{
+        //    health = value;
+        //}
+
+
         // метод созданный вне класса в других языках программирования называется функцией.
         // в С# только методы.
         public void Hit(int damage)
         {
-            if (damage > damage)
+            if (damage >= health)
             {
-                damage = health;
+                health = damage--;
             }
 
-            //health -= damage;
+            health -= damage;
 
-            Health -= damage;
+            //Health -= damage;
         }
     }
 }
--- src/CourseHunter_51_Classes/Program.cs
+++ src/CourseHunter_51_Classes/Program.cs
@@ -7,10 +7,17 @@ namespace CourseHunter_51_Classes
         static void Main(string[] args)
         {
             Character c = new Character();
-            c.Hit(10);
+            c.Hit(120);
 
 
-            Console.WriteLine($"Hello World! {c.Health}");
+            Console.WriteLine($"Hello World! {c.health}");
+
+            Calculator calc = new Calculator();
+            double triangleArea = calc.CalcTriangleArea(2, 5, 6);
+            double triangleArea2 = calc.CalcTriangleArea(2, 6);
+
+            Console.WriteLine($"{triangleArea} 1");
+            Console.WriteLine($"{triangleArea2} 2");
 
             Console.ReadLine();
         }
