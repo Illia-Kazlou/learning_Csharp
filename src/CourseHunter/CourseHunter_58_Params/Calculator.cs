09a84c94b36a687c6aaea0e6f4ffaf5a006488b7
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2021-02-19 18:29:47 +0300

    add CourseHunter_58_Params

---
+++ src/CourseHunter/CourseHunter_58_Params/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseHunter_58_Params
+{
+    public class Calculator
+    {
+        public double Average (int[] numbers)
+        {
+            double summ = 0;
+            foreach (var item in numbers)
+            {
+                summ += item;
+            }
+
+            return summ / numbers.Length;
+        }
+
+        public double Average2 (params int[] numbers)
+        {
+
+            double summ = 0;
+            foreach (var item in numbers)
+            {
+                summ += item;
+            }
+
+            return summ / numbers.Length;
+        }
+
+    }
+}
---
+++ src/CourseHunter/CourseHunter_58_Params/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CourseHunter_58_Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+
+            //для вызова простого метода из другого класса.
+            double avr = calc.Average(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            //для упращения вышеуказанной строки. Здесь массив создается ИМПЛИЦИДНО, т.е не явно)
+            double avr2 = calc.Average2( 1, 2, 3, 4, 5, 6, 7, 8, 9 );
+
+            Console.WriteLine(new string('_',35));
+
+            Console.WriteLine($"Average {avr}");
+            Console.WriteLine($"Average2 {avr2}");
+
+            Console.ReadLine();
+        }
+    }
+}
