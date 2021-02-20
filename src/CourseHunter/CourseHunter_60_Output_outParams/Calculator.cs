e86692ca3c3ff4216cff40592f1a9eb9643ba730
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2021-02-20 12:20:05 +0300

    add project CourseHunter_60_Output_outParams

---
+++ src/CourseHunter/CourseHunter_60_Output_outParams/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CourseHunter_60_Output_outParams
+{
+    public class Calculator
+    {
+        public bool TryDivide(double divisible, double divisor, out double result) //out параметр должен идти в конце, может быть больше одного.
+        {
+            result = 0;
+
+            if (divisor == 0)
+            {
+                return false;
+            }
+            else
+            {
+                result = divisible / divisor;
+                return true;
+            }
+        }
+
+        public double CalcTriangleArea(double sizeSideAB, double sizeSideBC, double sizeSideCA)
+        {
+            //Semiperimeter
+            double semiperimeter = (sizeSideAB + sizeSideBC + sizeSideCA) / 2;
+
+            return Math.Sqrt(semiperimeter * (semiperimeter - sizeSideAB) * (semiperimeter - sizeSideBC) * (semiperimeter - sizeSideCA));
+        }
+
+        public double CalcTriangleArea(double sizeH, double sizeSemihypotenuse)
+        {
+            return ((sizeSemihypotenuse / 2) * sizeH);
+        }
+
+        /// <summary>
+        /// Area of tringle.
+        /// </summary>
+        /// <param name="sizeSideAB">Size of side AB.</param>
+        /// <param name="sizeSideBC">Size of side BC.</param>
+        /// <param name="alpfa">Angle between side AB and BC.</param>
+        /// <param name="IsRadians">Type of angle.</param>
+        /// <returns></returns>
+        public double CalcTriangleArea(double sizeSideAB, double sizeSideBC, int alpfa, bool IsRadians = false)
+        {
+            double rads = alpfa * Math.PI * 180;
+            return 0.5 * sizeSideAB * sizeSideBC * Math.Sin(rads);
+        }
+    }
+}
---
+++ src/CourseHunter/CourseHunter_60_Output_outParams/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CourseHunter_60_Output_outParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+
+            string enterValue = Console.ReadLine();
+
+            //int number; // так надо было писать раньше теперь это не надо.
+
+            bool isParsed = int.TryParse(enterValue, out int number); // просто в out  указываешь тип переменной.
+
+            if (isParsed)
+            {
+            Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+
+            Console.WriteLine(new string('_', 35));
+
+            Calculator calc = new Calculator();
+
+            //помимо того что данный метод ВОЗВРАЩАЕТ bool, он также показывает нам результат выполнения.
+            if (calc.TryDivide(9,2, out double result))
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
