22fc3a7de02c41288c813a0b1b2af6e59345ce02
Illia Kozlov <fearofwar@mail.ru>
2021-02-22 12:35:14 +0300

    add project CourseHunter_64_StructureContainRefTypes

---
+++ src/CourseHunter/CourseHunter_64_StructureContainRefTypes/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace CourseHunter_64_StructureContainRefTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EvilStruct evilStruct = new EvilStruct();
+            evilStruct.PointRef = new PointRef();
+
+            evilStruct.X = 1;
+            evilStruct.Y = 2;
+            //либо
+            evilStruct.PointRef = new PointRef() { X = 1, Y = 2};
+
+            EvilStruct evilStruct2 = evilStruct;
+            Console.WriteLine($" evilStruct x = {evilStruct.PointRef.X} y = {evilStruct.PointRef.Y}");
+            Console.WriteLine($" evilStruct2 x = {evilStruct2.PointRef.X} y = {evilStruct2.PointRef.Y}");
+
+
+            evilStruct2.PointRef.X = 42;
+            evilStruct2.PointRef.Y = 45;
+
+            Console.WriteLine($" evilStruct x = {evilStruct.PointRef.X} y = {evilStruct.PointRef.Y}");
+            Console.WriteLine($" evilStruct2 x = {evilStruct2.PointRef.X} y = {evilStruct2.PointRef.Y}");
+
+
+
+            Console.WriteLine(new string('_', 40));
+            PointVal pointVal = new PointVal();  //или  PointVal pointVal;
+            pointVal.X = 10;
+            pointVal.Y = 30;
+
+            PointVal pointVal2 = pointVal;
+            pointVal2.X = 15;
+            pointVal2.Y = 35;
+
+            pointVal.LogValues();
+            pointVal2.LogValues();
+
+            //структура хранит в себе определенное значение память выделяется.
+            Console.WriteLine(new string('_', 40));
+
+            PointRef pointRef = new PointRef();  //или  PointVal pointVal;
+            pointRef.X = 10;
+            pointRef.Y = 30;
+
+            PointRef pointRef2 = pointRef;
+            pointRef2.X = 15;
+            pointRef2.Y = 35;
+
+            pointRef.LogValues();
+            pointRef2.LogValues();
+            //Класс это ссылочный элемент в памяти хранится ссылка на объект.
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/CourseHunter/CourseHunter_64_StructureContainRefTypes/ValRefTypes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CourseHunter_64_StructureContainRefTypes
+{
+    public struct EvilStruct
+    {
+        public int X;
+        public int Y;
+
+        public PointRef PointRef;
+    }
+
+    // Это структура
+    public struct PointVal
+    {
+        public int X;
+        public int Y;
+
+        public void LogValues()
+        {
+            Console.WriteLine($"x = {X}, e = {Y}");
+        }
+    }
+
+    public class PointRef
+    {
+        public int X;
+        public int Y;
+
+        public void LogValues()
+        {
+            Console.WriteLine($"x = {X}, e = {Y}");
+        }
+    }
+
+}
