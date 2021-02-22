9ad4cd90e9da9a7c61f9faf9a6419155001fd61d
Illia Kozlov <fearofwar@mail.ru>
2021-02-22 12:08:29 +0300

    add project CourseHunter_63_ValueReferenseTypes

---
+++ src/CourseHunter/CourseHunter_63_ValueReferenseTypes/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CourseHunter_63_ValueReferenseTypes
+{
+    public class Calculator
+    {
+        public double CalcTriangleArea(double sizeSideAB, double sizeSideBC, double sizeSideCA)
+        {
+            //Semiperimeter
+            double semiperimeter = (sizeSideAB + sizeSideBC + sizeSideCA) / 2;
+
+            //Area formula gerona
+            //double square = Math.Sqrt(semiperimeter * (semiperimeter - sizeSideAB) * (semiperimeter - sizeSideBC) * (semiperimeter - sizeSideCA));
+            //return square;
+            //Не обязательно новая переменная.
+
+            return Math.Sqrt(semiperimeter * (semiperimeter - sizeSideAB) * (semiperimeter - sizeSideBC) * (semiperimeter - sizeSideCA));
+        }
+
+        public double CalcTriangleArea(double sizeH, double sizeSemihypotenuse)
+        {
+            return ((sizeSemihypotenuse / 2) * sizeH);
+        }
+
+        /// <summary>
+        /// Area of tringle.
+        /// </summary>
+        /// <param name="sizeSideAB">Size of side AB.</param>
+        /// <param name="sizeSideBC">Size of side BC.</param>
+        /// <param name="alpfa">Angle between side AB and BC.</param>
+        /// <param name="IsRadians">Type of angle.</param>
+        /// <returns></returns>
+        //public double CalcTriangleArea(double sizeSideAB, double sizeSideBC, int alphfa )
+        //{
+        //    double rads = alphfa * Math.PI * 180;
+        //    return 0.5 * sizeSideAB * sizeSideBC * Math.Sin(rads);
+        //}
+
+        //Что бы не плодить и не делать зло по повторению кода. 
+        //По тому, что перегрузка это по сути копирование метода.
+        //В большенсте случает bool IsRadians = false. Это и есть опциональный параметр.
+        //ОГРАНИЧЕНИЯ: либо True or False или цифры.
+        //Опциональные пораметрвы идут крайние в методе.
+        public double CalcTriangleArea(double sizeSideAB, double sizeSideBC, int alphfa, bool IsRadians = false )
+        {
+            if (IsRadians)
+            {
+                Console.WriteLine("True");
+                return 0.5 * sizeSideAB * sizeSideBC * Math.Sin(alphfa);
+
+            }
+            else
+            {
+                double rads = alphfa * Math.PI * 180;
+                return 0.5 * sizeSideAB * sizeSideBC * Math.Sin(rads); 
+            }
+        }
+    }
+}
---
+++ src/CourseHunter/CourseHunter_63_ValueReferenseTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CourseHunter_63_ValueReferenseTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PointVal pointVal = new PointVal();  //или  PointVal pointVal;
+            pointVal.X = 10;
+            pointVal.Y = 30;
+
+            PointVal pointVal2 = pointVal;
+            pointVal2.X = 15;
+            pointVal2.Y = 35;
+
+            pointVal.LogValues();
+            pointVal2.LogValues();
+
+            //структура хранит в себе определенное значение память выделяется.
+            Console.WriteLine(new string('_', 40));
+
+            PointRef pointRef = new PointRef();  //или  PointVal pointVal;
+            pointRef.X = 10;
+            pointRef.Y = 30;
+
+            PointRef pointRef2 = pointRef;
+            pointRef2.X = 15;
+            pointRef2.Y = 35;
+
+            pointRef.LogValues();
+            pointRef2.LogValues();
+            //Класс это ссылочный элемент в памяти хранится ссылка на объект.
+
+            Console.ReadLine();
+        }
+    }
+}
