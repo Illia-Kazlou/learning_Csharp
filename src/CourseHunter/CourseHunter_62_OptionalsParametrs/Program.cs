5a52fa55d602143d33f29c7a957f50c7033347b6
Illia Kozlov <fearofwar@mail.ru>
2021-02-22 10:37:19 +0300

    add project CourseHunter_62_OptionalsParametrs

---
+++ src/CourseHunter/CourseHunter_62_OptionalsParametrs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CourseHunter_62_OptionalsParametrs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.CalcTriangleArea(10, 20, 30, true);
+
+
+            Console.ReadLine();
+        }
+    }
+}
