ba04c33537fe187b6eecc6a4ba2cb0bd477d2c68
Illia Kozlov <fearofwar@mail.ru>
2021-02-23 19:31:47 +0300

    add project CourseHunter_66_RefTypesAsArguments

---
+++ src/CourseHunter/CourseHunter_66_RefTypesAsArguments/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseHunter_66_RefTypesAsArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 15;
+            Swap(ref a, ref b); 
+            Console.WriteLine($"{a},{b}");
+
+
+            Console.WriteLine(new string('_', 35));
+            List<int> newList = new List<int>();
+            AddNumbers(newList);
+
+            foreach (var item in newList)
+            {
+                Console.Write(item + ";");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void AddNumbers(List<int> numbers)
+        {
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+        }
+
+        // Здесь мы будем работать с полноценной копией. И чтобы нам получить данные из метода REF.
+        static void Swap (ref int a, ref int b) 
+        {
+            int temp = a;
+            a = b;
+            b = temp; 
+        }
+    }
+}
