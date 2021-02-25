b0189a0dc76064945af177e0beb8a873ed83925e
Illia Kozlov <fearofwar@mail.ru>
2021-02-25 11:24:04 +0300

    add project CourseHunter_68_BoxingUnboxing

---
+++ src/CourseHunter/CourseHunter_68_BoxingUnboxing/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseHunter_68_BoxingUnboxing
+{
+    public struct PointVal
+    {
+        public int X;
+        public int Y;
+
+        public void LogValues()
+        {
+            Console.WriteLine($"x = {X}, e = {Y}");
+        }
+    }
+
+    public class PointRef
+    {
+        public int X;
+        public int Y;
+
+        public void LogValues()
+        {
+            Console.WriteLine($"x = {X}, e = {Y}");
+        }
+    }
+}
---
+++ src/CourseHunter/CourseHunter_68_BoxingUnboxing/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CourseHunter_68_BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            object boxing = x; //Процесс называется BOXING
+            int y = (int)boxing; //Процесс Unboxing c явным приведением типа 
+
+            double PI = 3.14;
+            object obj1 = PI;
+
+            int number = (int)(double)obj1; // Работать с object не безопасно из-за приведения типов
+            Console.WriteLine(number);
+
+
+
+
+            Console.ReadLine();
+        }
+
+        static void Do(object obj)
+        {
+            bool isPointRef = obj is PointRef; //проверяем содержит ли object класс PointRef. 1 - ВАРИАНТ
+            if (isPointRef)
+            {
+                PointRef pointRef = (PointRef)obj;
+                pointRef.X = 5;
+                pointRef.Y = 10;
+            }
+
+            PointRef pointRef1 = obj as PointRef;
+
+            if (pointRef1 != null)
+            {
+                Console.WriteLine(pointRef1.X);
+            }
+
+        }
+    }
+}
