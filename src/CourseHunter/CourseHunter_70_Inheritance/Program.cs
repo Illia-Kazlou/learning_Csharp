01c5f2530308f85a452b74676c13d9c351617518
Illia Kozlov <fearofwar@mail.ru>
2021-02-28 16:31:13 +0300

    add first part of project about inheritance

---
+++ src/CourseHunter/CourseHunter_70_Inheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CourseHunter_70_Inheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankTerminal bankTerminal = new BankTerminal("+21425+");
+            bankTerminal.Connect();
+
+            ModelXTermonal modelXTermonal = new ModelXTermonal("+214257+");
+            modelXTermonal.Connect();
+
+            ModelYTermonal modelYTermonal = new ModelYTermonal("+213565+");
+            modelYTermonal.Connect();
+
+            Console.ReadLine();
+        }
+    }
+}
