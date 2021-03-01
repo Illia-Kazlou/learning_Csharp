5022bc10a78af7d6bff0a6806173fac64bd3b22c
Illia Kozlov <fearofwar@mail.ru>
2021-03-01 19:47:33 +0300

    add project CourseHunter_71_Polymorphism

---
+++ src/CourseHunter/CourseHunter_71_Polymorphism/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CourseHunter_71_Polymorphism
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape [2];
+            shapes[0] = new Triangle(10, 20, 30);
+            shapes[1] = new Recanlge(10, 20);
+
+            foreach (Shape shape in shapes)
+            {
+                shape.Draw();
+                Console.WriteLine(shape.Preimeter());
+
+
+            }
+            
+
+
+            Console.WriteLine(new string ('_', 35));
+
+            BankTerminal bankTerminal = new BankTerminal("+21425+");
+            bankTerminal.Connect();
+
+            ModelXTermonal modelXTermonal = new ModelXTermonal("+214257+");
+            modelXTermonal.Connect();
+
+            ModelYTermonal modelYTermonal = new ModelYTermonal("+213565+");
+            modelYTermonal.Connect();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/CourseHunter/CourseHunter_71_Polymorphism/Shapes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace CourseHunter_71_Polymorphism
+{
+    public abstract class Shape
+    {
+        protected Shape()
+        {
+            Console.WriteLine("Shape created");
+        }
+
+        public abstract void Draw();
+
+        public abstract double Area();
+
+        public abstract double Preimeter();
+    }
+
+    public class Recanlge : Shape
+    {
+        private readonly double width;
+        private readonly double height;
+
+        public Recanlge(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+
+            Console.WriteLine("Rectangle created");
+        }
+
+        public override double Area()
+        {
+            return width * height;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing rectangle");
+        }
+
+        public override double Preimeter()
+        {
+            return 2 * (width + height);
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        private readonly double ab;
+        private readonly double ca;
+        private readonly double bc;
+
+        public Triangle(double ab, double bc, double ca)
+        {
+            this.ab = ab;
+            this.ca = ca;
+            this.bc = bc;
+
+            Console.WriteLine("Triengle created");
+        }
+
+        public override double Area()
+        {
+            double s = (ab + bc + ca) * 2;
+            return Math.Sqrt(s * (s - ab) * (s - bc) * (s - ca));
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing triengle");
+        }
+
+        public override double Preimeter()
+        {
+            return ab + bc + ca;
+        }
+    }
+}
