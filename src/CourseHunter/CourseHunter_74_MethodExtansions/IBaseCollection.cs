5bb6a3bc0dc77350beb7b626690a6bd81a9c821f
Illia Kozlov <fearofwar@mail.ru>
2021-03-03 16:59:56 +0300

    add progect CourseHunter_74_MethodExtansions

---
+++ src/CourseHunter/CourseHunter_74_MethodExtansions/IBaseCollection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace CourseHunter_74_MethodExtansions
+{
+    public interface IBaseCollection
+    {
+        void Add(object obj);
+
+        void Remove(object obj);
+
+        //void Clear(object obj);
+    }
+
+    public static class BaseCollectionExtansion
+    {
+        public static void AddRange (this IBaseCollection baseCollection, IEnumerable<int> objects)
+        {
+            foreach (var item in objects)
+            {
+                baseCollection.Add(item);
+            }
+        }
+    }
+
+    public static class BaseCallectionExtansion
+    {
+        public static void AddRange(this IBaseCollection baseCollection, IEnumerable<object> objects)
+        {
+            foreach (var item in objects)
+            {
+                baseCollection.Add(item);
+            }
+        }
+    }
+
+    public class BaseList : IBaseCollection
+    {
+        private object[] items;
+        private int counter = 0;
+
+        public BaseList( int initialCapasity)
+        {
+            items = new object[initialCapasity];
+        }
+
+        public void Add(object obj)
+        {
+            items[counter] = obj;
+            counter++;
+        }
+
+        public void Clear(object obj)
+        {
+            items[counter] = null;
+            counter--;
+        }
+
+        public void Remove(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/CourseHunter/CourseHunter_74_MethodExtansions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseHunter_74_MethodExtansions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> listInt = new List<object> { 1, 2, 3 };
+
+            IBaseCollection baseCollection = new BaseList(4);
+
+            baseCollection.AddRange(listInt);
+
+            baseCollection.Add(1);
+
+            Console.ReadLine();
+        }
+    }
+}
