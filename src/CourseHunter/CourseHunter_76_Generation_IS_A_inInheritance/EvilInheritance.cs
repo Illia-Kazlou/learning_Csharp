46898df3bfebd4d3ea75ef55a7b48b2bcf480cc6
Illia Kozlov <fearofwar@mail.ru>
2021-03-04 18:21:28 +0300

    [CursHunter] add lesson 76_Generation_IS_A_inInheritance

---
+++ src/CourseHunter/CourseHunter_76_Generation_IS_A_inInheritance/EvilInheritance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseHunter_76_Generation_IS_A_inInheritance
+{
+    public class Rect : IShape
+    {
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public int CalcSquare()
+        {
+            return Height * Width;
+        }
+    }
+
+    public class Square : IShape
+    {
+        public int SizeC { get; set; }
+
+        public int CalcSquare()
+        {
+            return SizeC * SizeC;
+        }
+    }
+
+    public static class AreaCalculator
+    {
+        //public static int CalcSquare(Square square)
+        //{
+        //    return square.Height * square.Height; 
+        //}
+
+        public static int CalcSquare(Rect rect)
+        {
+            return rect.Height * rect.Width;
+        }
+    }
+
+    // как лучше было сделать.
+    public interface IShape
+    {
+        int CalcSquare();
+    }
+
+
+}
---
+++ src/CourseHunter/CourseHunter_76_Generation_IS_A_inInheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CourseHunter_76_Generation_IS_A_inInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape rect = new Rect { Height = 6, Width = 12 };
+            IShape square = new Square { SizeC = 6 };
+
+            Console.WriteLine(rect.CalcSquare());
+            Console.WriteLine(square.CalcSquare());
+
+
+            //Rect rect = new Rect { Height = 6, Width = 12 };  //option inisiolazer
+            //int rectArea = AreaCalculator.CalcSquare(rect);
+            //Console.WriteLine($"Rect area = {rectArea}");
+
+            //Rect squre = new Square { Height = 6, Width = 12 }; //Representative по сути это не квадрат а экземпляр прямоугольника тип квадрат.
+            //int squareArea = AreaCalculator.CalcSquare(squre);
+            //Console.WriteLine($"Rect area = {squareArea}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
