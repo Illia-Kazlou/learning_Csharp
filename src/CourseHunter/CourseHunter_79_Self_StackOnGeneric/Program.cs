6995f0dc494ca8a0d42807cdf0da99dd6e095e2a
Illia Kozlov <fearofwar@mail.ru>
2021-03-12 10:25:52 +0300

    [CourseHunter] remake in lesson 78 object on Genereic

---
+++ src/CourseHunter/CourseHunter_79_Self_StackOnGeneric/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CourseHunter_79_Self_StackOnGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackBaseOnObect<int> stackBaseOnObect = new StackBaseOnObect<int>();
+            stackBaseOnObect.Push(1);
+            stackBaseOnObect.Push(3);
+            stackBaseOnObect.Push(2);
+            stackBaseOnObect.Push('r'); // здесь находится его цифровая реализация
+
+            // Invalid cast exeption
+            while (stackBaseOnObect.Count != 0)
+            {
+                Console.WriteLine(stackBaseOnObect.Peek());
+                stackBaseOnObect.Pop();
+            }
+
+
+            //Console.WriteLine(stackBaseOnObect.Peek());
+
+            //stackBaseOnObect.Pop();
+
+            //Console.WriteLine(stackBaseOnObect.Peek());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/CourseHunter/CourseHunter_79_Self_StackOnGeneric/StackBaseOnObect.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseHunter_79_Self_StackOnGeneric
+{
+    public class StackBaseOnObect<T>
+    {
+        private T[] _item;
+
+        public int Count { get; private set; }  // счетчик элементов в массиве или в нашем случае стеке. 
+        
+        public int Capasity
+        {
+            get
+            {
+                return _item.Length;
+            }
+        }
+
+        public StackBaseOnObect()
+        {
+            const int defaulfCapasity = 4;
+            _item = new T[defaulfCapasity];
+        }
+
+        public StackBaseOnObect(int capasity)
+        {
+            _item = new T[capasity];
+        }
+
+        public void Push (T item)
+        {
+            if (_item.Length == Count)
+            {
+                T[] largerArray = new T[Count * 2];
+                Array.Copy(_item, largerArray, Count);  // какой, куда, количество копируемых.
+
+                _item = largerArray;
+            }
+            _item[Count] = item;
+            Count++;
+        }
+
+        public void Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            _item[--Count] = default(T);  //null ------ здесь нельзя сказать какой тип  и не все типы поддерживают null.
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _item[Count - 1];
+        }
+
+    }
+}
