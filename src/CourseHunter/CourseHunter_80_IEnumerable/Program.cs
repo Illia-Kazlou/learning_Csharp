db0a2b85ee4938b0eb1e68a676bd5f878c702e94
Illia Kozlov <fearofwar@mail.ru>
2021-03-12 11:36:38 +0300

    [CourseHunter] lesson 80 IEnumerable

---
+++ src/CourseHunter/CourseHunter_80_IEnumerable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CourseHunter_80_IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackBaseOnObect<int> stackBaseOnObect = new StackBaseOnObect<int>();
+            stackBaseOnObect.Push(1);
+            stackBaseOnObect.Push(3);
+            stackBaseOnObect.Push(2);
+
+            //System.Collections.IEnumerable equatable = (System.Collections.IEnumerable)stackBaseOnObect;
+            //equatable.GetEnumerator(); // -------> вот так мы можем вызвать необощенный !!!!
+
+            // Допустим у нас есть похожие классы  и мы хотим использовать foreach.
+            ///foreach (var item in stackBaseOnObect)
+            ///{
+            ///}
+            // В данном случае мы не можем использовать foreach, так как это просто экземпляр класса.
+            // Для того что бы это работало нам необходимо в классе реализоватиь интерфейс IEnumerble.-------> Неправильное суждение, но ести и другой спосод
+
+            //while (stackBaseOnObect.Count != 0)
+            //{
+            //    Console.WriteLine(stackBaseOnObect.Peek());
+            //    stackBaseOnObect.Pop();
+            //}
+
+            foreach (var item in stackBaseOnObect)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/CourseHunter/CourseHunter_80_IEnumerable/StackBaseOnObect.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseHunter_80_IEnumerable
+{
+    public class StackBaseOnObect<T> : IEnumerable<T>
+    {
+        private T[] _item;
+
+        public int Count { get; private set; }  // счетчик элементов в массиве или в нашем случае стеке. 
+        
+        public int Capasity
+        {
+            get
+            {
+                return _item.Length;
+            }
+        }
+
+        public StackBaseOnObect()
+        {
+            const int defaulfCapasity = 4;
+            _item = new T[defaulfCapasity];
+        }
+
+        public StackBaseOnObect(int capasity)
+        {
+            _item = new T[capasity];
+        }
+
+        public void Push (T item)
+        {
+            if (_item.Length == Count)
+            {
+                T[] largerArray = new T[Count * 2];
+                Array.Copy(_item, largerArray, Count);  // какой, куда, количество копируемых.
+
+                _item = largerArray;
+            }
+            _item[Count] = item;
+            Count++;
+        }
+
+        public void Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            _item[--Count] = default(T);  //null ------ здесь нельзя сказать какой тип  и не все типы поддерживают null.
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return _item[Count - 1];
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new StackEnumerator<T>(_item);
+        }
+
+        // явная имплементация интерфейса.
+        // этот метод мы не будем видеть при создании экземпляра класса.
+        IEnumerator IEnumerable.GetEnumerator() // необобщенный. но мы его тоже можем вызвать иезуитским способом, смотри Program.cs.
+        {
+            // А здесь можно вызвать реализацию Generiково
+            return GetEnumerator();
+        }
+        // с помощи явной реализации интерфейса мы ее скрываем от внешнего пользователя.
+    }
+
+    // нам нужен экземпляр класса который реализует IEnumerator
+    public class StackEnumerator<T> : IEnumerator<T>
+    {
+        private readonly T[] array;
+        private int position = -1;
+
+        public StackEnumerator(T[] array)
+        {
+            this.array = array;
+        }
+
+        public T Current
+        {
+            get
+            {
+                return array[position];
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Dispose() // в 99% случаев он ничего не делает. Утилизировать(Удалить из памяти)
+        {
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return position < array.Length;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
