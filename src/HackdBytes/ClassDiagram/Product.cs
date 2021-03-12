2c006918be4c049cae4490b28c7e8fb78b0b30ab
Illia Kozlov <fearofwar@mail.ru>
2021-03-12 14:28:26 +0300

    [Hackd Bytes] init proj  -  https://www.youtube.com/watch?v=syYyQ7LlcPc&ab_channel=HackdBytes

---
+++ src/HackdBytes/ClassDiagram/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDiagram
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Category ProductCategory { get; set; }
+    }
+}
