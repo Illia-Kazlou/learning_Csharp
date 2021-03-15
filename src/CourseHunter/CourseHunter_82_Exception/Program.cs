f3f5db652f004e096c647765886aad54460ba5fd
Illia Kozlov <fearofwar@mail.ru>
2021-03-15 19:05:51 +0300

    [Course Hunter] add lesson 82 Exception

---
+++ src/CourseHunter/CourseHunter_82_Exception/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CourseHunter_82_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Все исключения это инстанции классов
+            // Базовый тип не oject а  exception.
+            Console.WriteLine("Please input a number");
+            string result = Console.ReadLine();
+
+            int number = 0;
+
+            try
+            {
+                number = int.Parse(result);
+            }
+            catch (FormatException ex)   // Отлавливаем исключение конкретно свяязанное с FormatException.
+            {
+                Console.WriteLine("A format exception has occured.");
+                Console.WriteLine("Informarion is below:");
+                Console.WriteLine(ex.ToString());
+            }
+            catch (Exception ex) // Отлавливаем все исключения так как это базовый тип.
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+    }
+}
