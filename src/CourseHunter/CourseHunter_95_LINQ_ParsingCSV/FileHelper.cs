76f132392d26e78ffab4cfd1840e06656a3812b9
Illia Kozlov <fearofwar@mail.ru>
2021-03-22 21:13:24 +0300

    [CourseHunter] add lesson 95_LINQ_ParsingCSV

---
+++ src/CourseHunter/CourseHunter_95_LINQ_ParsingCSV/FileHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CourseHunter_95_LINQ_ParsingCSV
+{
+    public class FileHelper
+    {
+        public static void MinMaxSummAverage(string path)
+        {
+            IEnumerable<CheesPlayer> list = File.ReadAllLines(path)
+                .Skip(1) // пропускаем первую строчку
+                .Select(x => CheesPlayer.ParseFideCSV(x)) // Метод расширения (ПРОЕКЦИИ). Берет объект и трансформирует в другой тип.
+                                                          //.Select(CheesPlayer.ParseFideCSV) // Синтаксический сахар верхней строки.
+
+                .Where(player => player.Country.Contains("NOR")) // страна
+                .OrderBy(player => player.Rank); // сортировка по 
+                //.Take(10);  // взять 10 
+                            //.ToList(); // метод расширения который IEnumerable превращает в лист.
+
+                int count = 1;
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{count} - {item}");
+                count++;
+            }
+        }
+
+    }
+}
---
+++ src/CourseHunter/CourseHunter_95_LINQ_ParsingCSV/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CourseHunter_95_LINQ_ParsingCSV
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHelper.MinMaxSummAverage("chess.csv");
+
+            Console.ReadLine();
+        }
+
+    }
+}
