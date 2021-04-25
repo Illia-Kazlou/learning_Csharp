6da3ee2452b6653c6c0dcb2211e2261c2bee44ff
Illia Kozlov <fearofwar@mail.ru>
2021-04-25 11:24:33 +0300

    [CourseHunter_27_DataTime] FOR with dateime

--- src/CourseHunter/CourseHunter_27_DataTime/Program.cs
+++ src/CourseHunter/CourseHunter_27_DataTime/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CourseHunter_27_DataTime
 {
@@ -22,6 +23,38 @@ namespace CourseHunter_27_DataTime
             Console.WriteLine(ts);  // as like ts=now.Subtract(dt)
             Console.WriteLine(new string('-', 30));
 
+
+            DateTime startDate = DateTime.Now;
+            DateTime finishDate = startDate.AddMonths(6);
+
+            Console.WriteLine($"{startDate} + 6 month {finishDate}");
+            Console.WriteLine(new string('-', 30));
+
+
+
+            static IEnumerable<DateTime> AllDatesBetween(DateTime start, DateTime end)
+            {
+                for (var day = start.Date; day <= end; day = day.AddDays(1))
+                    yield return day;
+            }
+
+            int count = 1;
+
+            //Console.WriteLine($"Start date is --- {startDate.AddMonths(-1)}");
+
+            //for (var i = startDate.AddMonths(1).Month; i <= finishDate.Month; i++)
+            //{
+            //    Console.WriteLine($"{count} month left --- {i}");
+            //    count++;
+            //}
+
+            for (var i = startDate.AddMonths(1); i <= finishDate; i = i.AddMonths(1))
+            {
+                Console.WriteLine($"{count} month left --- {i}");
+                count++;
+            }
+
+            Console.ReadLine();
         }
     }
 }
