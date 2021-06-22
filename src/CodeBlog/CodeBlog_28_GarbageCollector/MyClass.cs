83a5c13a71d93ff4c3529f920f669a2383aa81a8
Illia <66638336+Hamidalion@users.noreply.github.com>
2021-06-22 15:56:30 +0300

    [CodeBlog] add lesson 28 GC

---
+++ src/CodeBlog/CodeBlog_28_GarbageCollector/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlog_28_GarbageCollector
+{
+    public class MyClass : IDisposable
+    {
+        public MyClass() // Создание конструктора
+        {
+
+        }
+
+        ~MyClass() // Создание деструктора
+        { 
+            // Допустим при асинхронности используется для корректного закрытия потока
+        }
+
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+    }
+}
---
+++ src/CodeBlog/CodeBlog_28_GarbageCollector/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CodeBlog_28_GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                GC.Collect(4); //Вызов сборщика мусора
+                var memory = GC.GetTotalMemory(false);
+                Console.WriteLine($"{memory}");
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    var obj = (object)i;
+                    var j = (int)obj;
+                }
+
+                Console.WriteLine(new string ('_',15));
+                Console.WriteLine(GC.GetTotalMemory(false));
+
+                Console.WriteLine(new string('_', 15));
+                GC.Collect();
+                Console.WriteLine(GC.GetTotalMemory(false));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No 4");
+                throw;
+            }
+
+            using (var c = new MyClass()) // юсинг работает так как MyClass реализует интерфейс IDisposible
+            {
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
