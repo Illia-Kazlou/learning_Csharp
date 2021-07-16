d4b82b36107cb7960e646289618584d2aa30da0b
Illia <66638336+Hamidalion@users.noreply.github.com>
2021-07-16 17:26:17 +0300

    [Byte2plus] Events

---
+++ src/Byte++/Byte2plus_Events_/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Byte2plus_Events_
+{
+    public delegate void EventDelegate(); 
+
+    public class MyClass // для создания события нам необходим класс делегат
+    {
+        public event EventDelegate myEvent = null; //определение ивента, к делегату уже отношения не имеет
+    
+        public void InvokeEvent() // вызов события
+        {
+            myEvent.Invoke(); //это нужно дл ятого что бы мы могли из других классов запускать наши события
+        }
+    }
+}
--- src/Byte++/Byte2plus_Events_/Program.cs
+++ src/Byte++/Byte2plus_Events_/Program.cs
@@ -4,9 +4,29 @@ namespace Byte2plus_Events_
 {
     class Program
     {
+        static private void Handler1()
+        {
+            Console.WriteLine("Оброботчик событитя 1");
+        }
+
+        static private void Handler2()
+        {
+            Console.WriteLine("Handler event 2");
+        }
+
         static void Main(string[] args)
         {
-            //void delegate = new MyDelegate();
+            MyClass instanse = new MyClass();
+
+            // подписка на событие
+            instanse.myEvent += new EventDelegate(Handler1); //  техника предположения делегата
+            instanse.myEvent += Handler2;
+
+            Console.WriteLine(new string('-', 40));
+
+            //instanse.myEvent -= new EventDelegate(Handler2);
+
+            instanse.InvokeEvent();
 
 
             Console.ReadLine();
