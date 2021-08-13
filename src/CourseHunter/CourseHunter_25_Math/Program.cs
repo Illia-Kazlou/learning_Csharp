92ffecfd46d68c553a84eb5b501a0d4faae907a0
Illia <66638336+Hamidalion@users.noreply.github.com>
2021-08-13 10:00:20 +0300

    [CourseHunter_25_Math] add Ceiling method

--- src/CourseHunter/CourseHunter_25_Math/Program.cs
+++ src/CourseHunter/CourseHunter_25_Math/Program.cs
@@ -15,10 +15,21 @@ namespace CourseHunter_25_Math
             Console.WriteLine(Math.Round(1.7));
             Console.WriteLine();
 
+            Console.WriteLine(new string('=', 50));
+
             Console.WriteLine(Math.Round(1.5, MidpointRounding.AwayFromZero));  //округление
             Console.WriteLine(Math.Round(1.5, MidpointRounding.ToEven));
             Console.WriteLine(Math.Round(1.5, MidpointRounding.ToNegativeInfinity));
             Console.WriteLine(Math.Round(1.5, MidpointRounding.ToPositiveInfinity));
+
+            Console.WriteLine(new string('=', 50));
+
+            //double a = 1.5;
+
+            Console.WriteLine(Math.Ceiling(1.5)); // округление до большего числа всегда если есть цифра после запятой
+            Console.WriteLine(Math.Ceiling(1.4));
+            Console.WriteLine(Math.Ceiling(1.3));
+            Console.WriteLine(Math.Ceiling(1.2));
         }
     }
 }
