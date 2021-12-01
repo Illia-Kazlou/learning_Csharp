b05409e8bb922fa6ec21f7a8d5010f459645b126
Illia <66638336+Hamidalion@users.noreply.github.com>
2021-12-01 20:42:42 +0300

    Add comment

--- src/Metanit/Metanit_IEnumerable/Program.cs
+++ src/Metanit/Metanit_IEnumerable/Program.cs
@@ -5,7 +5,7 @@ namespace Metanit_IEnumerable
     class Program
     {
         /// <summary>
-        /// 
+        /// В примерах выше использовались необобщенные версии интерфейсов, однако мы также можем использовать их обобщенные двойники
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
